3b794d709ecce04d78a214c3ad856e8bc2a65ce2
Souvik Chakraborty <Souvikc3030@gmail.com>
2023-09-11 17:53:56 +0530

    added ball and bounce logic with wall

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from turtle import  Screen
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from time import sleep
 
+
+l_paddle = Paddle((350, 0))
+r_paddle = Paddle((-350, 0))
+ball = Ball()
 screen = Screen()
 screen.title("Pong Game")
 screen.bgcolor("black")
+screen.tracer(0)
 screen.setup(width=800, height=600)
+
+screen.listen()
+screen.onkey(l_paddle.go_up, "Up")
+screen.onkey(l_paddle.go_down, "Down")
+
+screen.onkey(r_paddle.go_up, "w")
+screen.onkey(r_paddle.go_down, "s")
+game_is_on = True
+while game_is_on:
+	sleep(0.1)
+	screen.update()
+	ball.move()
+
+	# detect collision with wall
+	if ball.ycor() > 280 or ball.ycor() < -280:
+		# need to bounce
+		ball.bounce()
 
 screen.exitonclick()
--- todos.md
+++ todos.md
@@ -1,9 +1,14 @@
-Todos:
+# Todos:
 - [x] Create the screen
-- [ ] Create and move paddle
-- [ ] Create another paddle
-- [ ] Create the ball and make it move
-- [ ] Detect collision with wall and bounce
+- [x] Create and move paddle
+  - width=20, height=100
+  - x_pos=350, y_pos=0
+
+- [x] Create another paddle
+- [x] Create the ball and make it move
+  - width=20, height=20
+  - x_pos=0, y_pos=0
+- [x] Detect collision with wall and bounce
 - [ ] Detect collision with paddle
 - [ ] Detect when paddle misses
 - [ ] Keep score
